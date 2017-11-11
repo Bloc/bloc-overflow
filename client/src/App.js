a6404b7fd87a3db636a002173ee3939ce8218e2e
Kara <kara.a.flaherty@gmail.com>
2017-11-11 12:09:28 -0700

    Fixed question link, added question read page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 import HomePage from './HomePage'
 import OtherPage from './OtherPage'
+import QuestionRead from './QuestionRead'
 import { Container, Header, Segment, Button, Icon, Dimmer, Loader, Divider } from 'semantic-ui-react'
 
 class App extends Component {
@@ -51,6 +52,9 @@ class App extends Component {
         )}/>
         <Route path='/other' render={() => (
           <OtherPage />
+        )}/>
+        <Route path='/questions/:id' render={() => (
+          <QuestionRead />
         )}/>
       </Container>
     : <Container text>
---
+++ client/src/QuestionRead.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Container } from 'semantic-ui-react'
+
+class QuestionRead extends Component {
+  render() {
+    return (
+      <Container>
+              Hello, world!
+      </Container>
+    )
+  }
+}
+
+export default QuestionRead
